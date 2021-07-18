c23ef171ae48bfbb3d95b567fa1ef78e2ba9a2db
A-jha383 <ayushj383@gmail.com>
2021-07-18 05:54:56 +0530

    code file

---
+++ app/src/main/java/com/example/android/protect_me/Mytrackers.java
@@ -0,0 +1,14 @@
+package com.example.android.protect_me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mytrackers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mytrackers);
+    }
+}
